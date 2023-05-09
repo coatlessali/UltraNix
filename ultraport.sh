a8835c64d79dc7f64a7ea7e83806de85fc0d8155
coatlessali <coatlessali@protonmail.com>
2023-05-08 20:05:48 -0500

    add darwin support

--- ultraport.sh
+++ ultraport.sh
@@ -1,33 +1,66 @@
 #!/bin/bash
 
-if [ -f "libsteam_api.so" ]; then
-    echo "./libsteam_api.so found!"
-else 
-    echo "libsteam_api.so not found in this folder! Please add it and try again."
-    exit
-fi
+if [[ "$(uname -a)" == *"Darwin"* ]]; then
+    echo "Darwin found in uname, assuming MacOS..."
+    if [ -f "ULTRAKILL.exe" ]; then
+        echo "Found ULTRAKILL executable..."
+    else
+        echo "Failed to locate ULTRAKILL executable, exiting."
+        exit
+    fi
 
-if [ -f "discord_game_sdk.so" ]; then
-	echo "./discord_game_sdk.so found!"
+    if [ -f "Darwin.tar.gz" ]; then
+        echo "Found Darwin.tar.gz..."
+        tar -xzf Darwin.tar.gz
+    else
+        echo "Failed to locate Darwin.tar.gz, exiting."
+        exit
+    fi
+    cp -r ULTRAKILL_Data/* ULTRAKILL.app/Contents/Resources/Data/
+    mv ULTRAKILL.app/Contents/Resources/Data/Resources/unity\ default\ resources ULTRAKILL.app/Contents/Resources/
+    mkdir -p ULTRAKILL.app/Contents/MonoBleedingEdge/osx/
+    if [ -f "discord_game_sdk.dylib" ]; then
+        echo "Found discord_game_sdk.dylib..."
+        mv discord_game_sdk.dylib ULTRAKILL.app/Contents/MonoBleedingEdge/osx/
+    fi
+    if [ -f "libsteam_api.dylib" ]; then
+        echo "Found libsteam_api.dylib..."
+        mv libsteam_api.dylib ULTRAKILL.app/Contents/MonoBleedingEdge/osx/libsteam_api64.dylib
+    fi
+    echo "Symlinking Preferences and Saves..."
+    ln -s "$(pwd)/Preferences" "$(pwd)/ULTRAKILL.app/Preferences"
+    ln -s "$(pwd)/Saves" "$(pwd)/ULTRAKILL.app/Saves"
+    echo "You can now open ULTRAKILL.app by right-clicking it and clicking \"Open\"."
 else
+    echo "Darwin not found in uname, assuming Linux/FreeBSD through Linuxulator..."
+    if [ -f "libsteam_api.so" ]; then
+        echo "Found libsteam_api.so..."
+        cp libsteam_api.so ULTRAKILL_Data/Plugins/x86_64/steam_api64.so
+    else
+        echo "libsteam_api.so not found, skipping..."
+    fi
+
+    if [ -f "discord_game_sdk.so" ]; then
+	echo "Found ./discord_game_sdk.so..."
+        cp discord_game_sdk.so ULTRAKILL_Data/Plugins/x86_64
+    else
 	echo "discord_game_sdk.so not found in this folder! Please add it and try again."
 	exit
+    fi
+
+    if [ -f "ULTRAPORT.tar.gz" ]; then
+	echo "Found ./ULTRAPORT.tar.gz..."
+        tar -xzf ULTRAPORT.tar.gz
+    else
+	echo "Failed to locate ./ULTRAPORT.tar.gz, exiting."
+    fi
+
+    echo "Linking Cybergrind to CyberGrind to avoid case sensitivity bug..."
+
+    ln -s Cybergrind CyberGrind
+
+    echo "Done! Either launch the game with ./ULTRAKILL.x86_64, or change your Steam launch options to the following to play!"
+
+    echo "\"$(pwd)/ULTRAKILL.x86_64 && echo %command%\""
+
 fi
-
-if [ -f "ULTRAPORT.tar.gz" ]; then
-	echo "./ULTRAPORT.tar.gz found!"
-else
-	echo "./ULTRAPORT.tar.gz not found in this folder! Please add it and try again."
-fi
-
-tar -xzf ULTRAPORT.tar.gz
-
-mv libsteam_api.so ULTRAKILL_Data/Plugins/x86_64/steam_api64.so
-
-mv discord_game_sdk.so ULTRAKILL_Data/Plugins/x86_64/
-
-ln -s Cybergrind CyberGrind
-
-echo "Done! Either launch the game with ./ULTRAKILL.x86_64, or change your Steam launch options to the following to play!"
-
-echo "\"$(pwd)/ULTRAKILL.x86_64 && echo %command%\""
